3642d150f6d73fe43a36d5889fcddf7183a0ac90
lafry5 <lafry5@yahoo.com>
2020-06-27 18:46:10 -0700

    updated loop for local storage

--- script2.js
+++ script2.js
@@ -1,37 +1,53 @@
-var rightNow = moment();
+// var currentDay = moment().format('LL');
+var currentHour = moment().hours();
+var j = 0;
 
+// if user hit save the description to local storage
 $(document).ready(function() {
-  // listen for save button clicks
-  $(".saveBtn").on("click", function() {
-    // get nearby values
-    var value = $(this).siblings(".description").val();
-    var time = $(this).parent().attr("id");
-
-    // save in localStorage
-    localStorage.setItem(time, value);
-  });
-
-  function hourUpdater() {
-    // get current number of hours
-    var currentHour = moment().hours();
-
-    // loop over time blocks
-    $(".time-block").each(function() {
-      var blockHour = parseInt($(this).attr("id").split("-")[1]);
-
-      // check if we've moved past this time
-      if (blockHour < currentHour) {
-        $(this).addClass("past");
-      } 
-      else if (blockHour === currentHour) {
-        $(this).removeClass("past");
-        $(this).addClass("present");
-      } 
-      else {
-        $(this).removeClass("past");
-        $(this).removeClass("present");
-        $(this).addClass("future");
-      }
+  var j = 0;
+  //syntax to select an element using Jquery
+  var btnArray = $(".saveBtn");
+  for (let i = 0; i < btnArray.length; i++) {
+    j = i + 9;
+    btnArray[i].addEventListener("click", function() {
+      j = i + 9;
+      //class for the button
+      // save description to local storage; i.e. how to get an element using Jquery
+      var textvalue = $("#data-" + j).val();
+      console.log(j);
+      localStorage.setItem("hour-" + j, textvalue); // hour is what is in local storage
     });
+
+    // Get data from local storage and display on the webpage
+    var textvalue = localStorage.getItem("hour-" + j);
+    $("#data-" + j).val(textvalue); //how to change value of elements in jquery; data is where we want to put the data
   }
-})
+  var currentDay = $("#currentDay").text(moment().format("dddd, MMMM Do, YYYY"));
+});
+
+// moment().format('MMMM Do YYYY, h:mm:ss a'); Need to extract the hour, 'h' for hourformatter
+
+hourformatter();
+
+function hourformatter() {
+  var blockhourArray = $(".description");
+  console.log('blockhourArray is '+blockhourArray);
+ 
+  var i =0;
+
+   for (let i = 9; i < blockhourArray.length; i++) {
+
+      if (blockhourArray[i] === currentHour) {
+          // if it is the same then textarea background = red
+          // #hour-9="background-color:#ff0000";  // this line doesn't work
+        }
+        else if (blockhourArray < currentHour) {
+            // textarea background = grey
+        }
+        else {
+            // textarea background = green
+        } //end of if blockhourArray
+        
+    }    
+} // end of hourformatter function
+
--- script3.js
+++ script3.js
@@ -1,26 +1,73 @@
-// var rightNow = moment();
+var currentDay = moment().format('LL');
+var currentHour = moment().hours();
 var j = 0;
 
 // if user hit save the description to local storage
 $(document).ready(function(){  //syntax to select an element using Jquery
-    var btnArray = $(".saveBtn");
-    for (var i=0; i<=btnArray.length; i++) { 
-
-        btnArray[i].click(function(){  //class for the button
+     var btnArray = $(".saveBtn");
+     console.log(btnArray);
+     
+     for (var i=0; i<=btnArray.length; i++) {  
+        $(".saveBtn").click(function(){  //class for the button
+        btnArray[i];
         // save description to local storage; i.e. how to get an element using Jquery
-        var j = i+9;
-        var textvalue = $("#data-"+j).val();
-        localStorage.setItem("hour-"+j, textvalue);   // hour is what is in local storage
-        });
+        //var j = i+9;
+    
+        var textvalue = $("#data-9").val();
+        localStorage.setItem("hour-9", textvalue);   // hour is what is in local storage
+    
+    }); // end of saveBtn click function
 
         // Get data from local storage and display on the webpage
-        var textvalue = localStorage.getItem("hour-"+j);
-        $("#data-"+j).val("test");  //how to change value of elements in jquery; data is where we want to put the data
+        var textvalue2 = localStorage.getItem("hour-9");
+        $("#data-9").val(textvalue2);  //how to change value of elements in jquery; data is where we want to put the data
+
+        // end of hour 9
+
+        $(".saveBtn").click(function(){  
+            btnArray[i];
+        
+            var textvalue = $("#data-10").val();
+            localStorage.setItem("hour-10", textvalue);   
+
+        }); // end of saveBtn click function
     
-    }
-  });
+             var textvalue2 = localStorage.getItem("hour-10");
+            $("#data-10").val(textvalue2);  
+   
+        // end of hour 10
+        
+        $(".saveBtn").click(function(){  
+            btnArray[i];
+        
+            var textvalue = $("#data-11").val();
+            localStorage.setItem("hour-11", textvalue);   
+
+        }); // end of saveBtn click function
+    
+             var textvalue2 = localStorage.getItem("hour-11");
+            $("#data-11").val(textvalue2);  
+   
+        // end of hour 11  
+
+  } // end of for loop
+
+});   // end of document ready function
   
-
-
+hourformatter();
+  
   $("#currentDay").text(moment().format('dddd, MMMM Do, YYYY'));
 
+  function hourformatter() {
+    var blockhour = ("#data-9");
+    console.log('blockhour is '+blockhour);
+    if (blockhour === currentHour) {
+        #hour-9="background-color:#ff0000";  // this line doesn't work
+    }
+    else if (blockhour < currentHour) {
+        // textarea background = grey
+    }
+    else {
+        // textarea background = green
+    } 
+  } // end of hourformatter function
