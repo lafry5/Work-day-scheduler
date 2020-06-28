ad05fed5a6d9f68e38b01d2b66ffa1693d4d79db
lafry5 <lafry5@yahoo.com>
2020-06-27 19:19:48 -0700

    finished

--- script2.js
+++ script2.js
@@ -3,13 +3,13 @@ var currentHour = moment().hours();
 var j = 0;
 
 // if user hit save the description to local storage
-$(document).ready(function() {
+$(document).ready(function () {
   var j = 0;
   //syntax to select an element using Jquery
   var btnArray = $(".saveBtn");
   for (let i = 0; i < btnArray.length; i++) {
     j = i + 9;
-    btnArray[i].addEventListener("click", function() {
+    btnArray[i].addEventListener("click", function () {
       j = i + 9;
       //class for the button
       // save description to local storage; i.e. how to get an element using Jquery
@@ -23,31 +23,22 @@ $(document).ready(function() {
     $("#data-" + j).val(textvalue); //how to change value of elements in jquery; data is where we want to put the data
   }
   var currentDay = $("#currentDay").text(moment().format("dddd, MMMM Do, YYYY"));
-});
+  
+  hourformatter();
 
-// moment().format('MMMM Do YYYY, h:mm:ss a'); Need to extract the hour, 'h' for hourformatter
+  function hourformatter() {
 
-hourformatter();
+    $(".time-block").each(function(){ //hour-9 => [hour, 9]
+      var blockHour = parseInt($(this).attr("id").split("-")[1]);// "9" => 9
+      console.log("block hour", blockHour);
+      if(blockHour < currentHour) {
+          $(this).addClass("past");
+      } else if(blockHour === currentHour) {
+          $(this).addClass("present");
+      } else {
+          $(this).addClass("future");
+      } // end of if
+    }); // end of time block
+  } // end of hour formatter       
 
-function hourformatter() {
-  var blockhourArray = $(".description");
-  console.log('blockhourArray is '+blockhourArray);
- 
-  var i =0;
-
-   for (let i = 9; i < blockhourArray.length; i++) {
-
-      if (blockhourArray[i] === currentHour) {
-          // if it is the same then textarea background = red
-          // #hour-9="background-color:#ff0000";  // this line doesn't work
-        }
-        else if (blockhourArray < currentHour) {
-            // textarea background = grey
-        }
-        else {
-            // textarea background = green
-        } //end of if blockhourArray
-        
-    }    
-} // end of hourformatter function
-
+}); //was there
